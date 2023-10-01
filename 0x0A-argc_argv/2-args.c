5e1b441a3127f84f614bb1b90e10a4bd041b4974
Dev-Kings <kingasiadavid41@gmail.com>
2023-10-01 00:51:14 -0700

    prints args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints arguments paased to main
+ *
+ * @argc: number of arguments
+ * @argv: array of pointer to arguments
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int count;
+
+	for(count = 0; count < argc; count++)
+	{
+		printf("%s\n", argv[count]);
+	}
+	return (0);
+}

df9e50dc23ae03052141e1ad1c77c0c89d57baa5
Dev-Kings <kingasiadavid41@gmail.com>
2023-10-10 11:04:56 -0700

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct_dog - dog info
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ * Description: A longer description.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif

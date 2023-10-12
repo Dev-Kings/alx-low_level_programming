09c454cb2ad2bac46307812415f5775425a38699
Dev-Kings <kingasiadavid41@gmail.com>
2023-10-12 05:33:26 -0700

    changed separator null logic

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,18 +14,15 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list lst;
 	unsigned int i;
 
-	if (separator != NULL)
+	va_start(lst, n);
+	for (i = 0; i < n; i++)
 	{
-		va_start(lst, n);
-		for (i = 0; i < n; i++)
+		printf("%d", va_arg(lst, int));
+		if (i != (n - 1) && separator != NULL)
 		{
-			printf("%d", va_arg(lst, int));
-			if (i != (n - 1) && separator != NULL)
-			{
-				printf("%s", separator);
-			}
+			printf("%s", separator);
 		}
-		va_end(lst);
-		printf("\n");
 	}
+	va_end(lst);
+	printf("\n");
 }

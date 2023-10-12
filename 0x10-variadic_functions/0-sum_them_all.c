44e5a6bd85febb75da728d5d963e28e21b8618e9
Dev-Kings <kingasiadavid41@gmail.com>
2023-10-12 04:56:34 -0700

    moved assignment

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,12 +12,12 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	va_list lst;
 
+	sum = 0;
 	if (n == 0)
 	{
 		return (0);
 	}
-	
-	sum = 0;
+
 	va_start(lst, n);
 	for (i = 0; i < n; i++)
 	{

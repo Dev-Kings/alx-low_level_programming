de21225cf3435e764b023e74c11fbd513eabd646
Dev-Kings <kingasiadavid41@gmail.com>
2023-12-15 03:33:15 -0800

    handled out of range cases

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -21,7 +21,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		dprintf(2, "Error: Can't malloc\n");
 		return (NULL);
 	}
-	if (current == NULL)
+	if (current == NULL && idx == 0)
 	{
 		new_node->n = n;
 		new_node->prev = NULL;
@@ -34,10 +34,25 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		node_position++;
 		current = current->next;
 	}
+	if (node_position != idx)
+	{
+		free(new_node);
+		return (NULL);
+	}
 	new_node->n = n;
-	new_node->next = current;
-	new_node->prev = current->prev;
-	(current->prev)->next = new_node;
-	current->prev = new_node;
+	if (current == NULL)
+	{
+		new_node->prev = *h;
+		new_node->next = NULL;
+		(*h)->next = new_node;
+	}
+	else
+	{
+		new_node->prev = current->prev;
+		new_node->next = current;
+		if (current->prev != NULL)
+			(current->prev)->next = new_node;
+		current->prev = new_node;
+	}
 	return (new_node);
 }

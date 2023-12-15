a76a188c7cd7d29248f96da634d10202dfaba1f3
Dev-Kings <kingasiadavid41@gmail.com>
2023-12-15 03:11:47 -0800

    removed insertion at head

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -21,19 +21,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		dprintf(2, "Error: Can't malloc\n");
 		return (NULL);
 	}
-	new_node->n = n;
-	if (current == NULL)
-	{
-		new_node->next = NULL;
-		new_node->prev = NULL;
-		*h = new_node;
-		return (new_node);
-	}
 	while (current != NULL && node_position != idx)
 	{
 		node_position++;
 		current = current->next;
 	}
+	new_node->n = n;
 	new_node->next = current;
 	new_node->prev = current->prev;
 	(current->prev)->next = new_node;

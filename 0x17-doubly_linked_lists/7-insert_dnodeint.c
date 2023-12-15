bee5aee8076367447de568ffc9fc103d01b6b643
Dev-Kings <kingasiadavid41@gmail.com>
2023-12-15 03:15:51 -0800

    added test case for empty list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -21,6 +21,14 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		dprintf(2, "Error: Can't malloc\n");
 		return (NULL);
 	}
+	if (current == NULL)
+	{
+		new_node->n = n;
+		new_node->prev = NULL;
+		new_node->next = NULL;
+		*h = new_node;
+		return (new_node);
+	}
 	while (current != NULL && node_position != idx)
 	{
 		node_position++;

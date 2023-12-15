b6d02051177a4f7f8c4599177982f89855dec76d
Dev-Kings <kingasiadavid41@gmail.com>
2023-12-15 04:26:04 -0800

    test on 2 elements

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -35,7 +35,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		node_position++;
 		current = current->next;
 	}
-	if (node_position + 1 != idx)
+	if (node_position + 1 < idx)
 	{
 		free(new_node);
 		return (NULL);

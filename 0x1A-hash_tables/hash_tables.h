3778bd1ed7318e9206fbf6bd4ea3598bd9981daf
Dev-Kings <kingasiadavid41@gmail.com>
2024-04-13 17:11:13 +0300

    implemented function to give index of a key

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,6 +1,10 @@
 #ifndef HASH_TABLES
 #define HASH_TABLES
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct hash_node_s - Node of a hash table
  * @key: The Key, string
@@ -31,5 +35,6 @@ typedef struct hash_table_s
 
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
 
 #endif

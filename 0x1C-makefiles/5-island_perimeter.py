f4916c962ade4dc1185ff2ea91cea1ad35798756
Dev-Kings <kingasiadavid41@gmail.com>
2024-05-29 21:57:27 +0300

    refactored indentation

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -25,11 +25,11 @@ def island_perimeter(grid):
         for j in range(width):
             if grid[i][j] == 1:
                 size += 1
-            if (j > 0 and grid[i][j - 1] == 1):
-                edges += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
 
-            if (i > 0 and grid[i - 1][j] == 1):
-                edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
 
-perimeter = size * 4 - edges * 2
-return (perimeter)
+    perimeter = size * 4 - edges * 2
+    return (perimeter)
